c8c4b052f9cd8d869366360fa51868a277ceffef
Rajguru-Aditya <rajguru.aditya.25@gmail.com>
2022-08-16 18:09:32 +0530

    Using express

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+//listenn for requests on port 3000
+app.listen(3000);
+
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
